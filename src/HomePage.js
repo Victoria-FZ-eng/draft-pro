7d185bcd2b69aeb80cda356a49144a2705e171e7
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-28 10:31:53 +0300

    step2

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
 import UserInfoForm from './UserInfoForm';
 import axios from 'axios';
+import Slider from './Slider.js';
 
 class Home extends React.Component{
 
@@ -87,6 +88,7 @@ class Home extends React.Component{
             <>
             <h1>Hello In Home Page</h1>
             <UserInfoForm show={this.state.show}  hide={this.hideModal} saveUser={this.saveUsersInfo} />
+            <Slider />
             </>
         )
     }
---
+++ src/Slider.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import Button from 'react-bootstrap/Button';
+import { withAuth0 } from '@auth0/auth0-react';
+
+
+class Slider extends React.Component{
+    render(){
+        return(
+            <>
+
+
+            </>
+        )
+    }
+}
+
+export default withAuth0(Slider);
